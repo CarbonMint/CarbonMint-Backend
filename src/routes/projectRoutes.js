0704f381d6bbcf5b1f1f0e4b76c4fbdfc465592c
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:23:10 +0530

    feat(routes): add project routes

---
+++ src/routes/projectRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const projectController = require('../controllers/projectController');
+const asyncHandler = require('../utils/asyncHandler');
+
+const router = express.Router();
+
+// GET /api/projects
+router.get('/', asyncHandler(projectController.listProjects));
+
+// GET /api/projects/:id
+router.get('/:id', asyncHandler(projectController.getProject));
+
+module.exports = router;
