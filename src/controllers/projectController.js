d8e910e47a559cd544550c57570dd88b95e79aa7
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:22:04 +0530

    feat(controllers): add projectController

---
+++ src/controllers/projectController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const projectService = require('../services/projectService');
+
+/** GET /api/projects */
+function listProjects(_req, res) {
+  const projects = projectService.listProjects();
+  res.json({ count: projects.length, projects });
+}
+
+/** GET /api/projects/:id */
+function getProject(req, res) {
+  const project = projectService.getProject(req.params.id);
+  res.json({ project });
+}
+
+module.exports = { listProjects, getProject };
