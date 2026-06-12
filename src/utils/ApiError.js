b52d3ed5e75678baab75d2338e9002b389e9a08f
arisu6804 <arisu6804@gmail.com>
2026-06-12 18:19:14 +0530

    feat(utils): add ApiError class for HTTP errors

---
+++ src/utils/ApiError.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Operational error carrying an HTTP status code. Thrown by services and
+ * controllers and translated into a JSON response by the errorHandler
+ * middleware.
+ */
+class ApiError extends Error {
+  constructor(statusCode, message, details) {
+    super(message);
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+    this.details = details;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  static badRequest(message, details) {
+    return new ApiError(400, message, details);
+  }
+
+  static notFound(message = 'Resource not found') {
+    return new ApiError(404, message);
+  }
+
+  static conflict(message) {
+    return new ApiError(409, message);
+  }
+}
+
+module.exports = ApiError;
